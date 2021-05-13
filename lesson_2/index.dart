1cb975cf274e5d1d20cd6ca1cc2aeabfda6caa12
cuishijie <cuishijie@inke.cn>
2021-05-13 09:26:56 +0800

    up

---
+++ lesson_2/index.dart
@@ -0,0 +1,93 @@
+main() {
+  // start
+  // 循环 for for in |  while  do while
+  // for for in 确定循环
+  // for (var i = 0; i < 10; i++) {
+  //   print('${i}');
+  // }
+
+  // for in
+  // var obj = [1, 2]; // List
+
+  // for (var prop in obj) {
+  //   print(prop);
+  // }
+  // 执行到对象的所有属性都用完为止。
+
+  // while do while 不确定循环
+  // var a = 0;
+
+  // while(a < 10) { // 表达式或值 必须返回Boolean
+  //   a++;
+  // }
+  // print(a);
+
+  // var a = 0;
+
+  // do {
+  //   a++;
+  // }
+  // while (a < 10);
+  // print(a);
+  // do while 会至少执行一次。
+
+  // break 用来控制循环结构，
+  // for (var i = 0; i < 10; i++) {
+  //   if (i == 2) {
+  //     break;
+  //   }
+  //   print(i);
+  // }
+  // 执行这里 都有break;
+
+  // continue;
+  // 跳过当前的迭代语句，并且从下次循环的开始继续。
+  // for (var i = 0; i < 10; i++) {
+  //   if (i % 2 == 0) {
+  //     continue;
+  //   }
+  //   print(i);
+  // }
+
+  // 条件判断语句
+  // if  if else else if
+  // var a = 1;
+  // var b = 2;
+  // if (a < b) { // 判断条件的表达式或者值 必须是Boolean
+  //   print('a is bigger');
+  // } else {
+  //   print('b is bigger');
+  // }
+  // if (a < b) { // 判断条件的表达式或者值 必须是Boolean
+  //   print('a is bigger');
+  // }
+  // else if (b > 10) {
+  //   print('b is very bigger');
+  // }
+  // else if (b > 10) {
+  //   print('b is very bigger');
+  // }
+  // 其中如果有if条件通过了，那么后面的模块不执行了
+
+  // switch
+
+  // var a = 2;
+  // var b = 1; // Dart 专门的常量数据类型 const final
+  // var c = 0;
+
+  // switch(b + c) {
+  //   case 1:
+  //   case 2:
+  //     print('right');
+  //     break;
+  //   case 3:
+  //     print('right copy');
+  //     break;
+  //   default:
+  //     print('wrong');
+  // }
+  // switch case 表达式或值的类型必须是一样的
+  // default case 必须放在所有case的最后面
+
+  // END
+}
